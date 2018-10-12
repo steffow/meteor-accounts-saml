a290096174bef00f0c98129021a0062e09a6154a
iamshaunjpg <shaun@shaun-pelling>
2018-10-12 23:46:11 +0200

    Prep package.js for publish

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-    name:"steffo:meteor-accounts-saml",
+    name:"harry97:meteor-accounts-saml",
     summary: "SAML Login (SP) for Meteor. Works with OpenAM, OpenIDP and provides Single Logout.",
     version: "0.0.16",
-    git: "https://github.com/steffow/meteor-accounts-saml.git"
+    git: "https://github.com/harry97/meteor-accounts-saml.git"
 });
 
 Package.on_use(function (api) {
